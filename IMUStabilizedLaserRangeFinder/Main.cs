bdccfd6b641cdb633264f975584ce0a5651f7f5a
Patricio Palma <mail@ppalma.cl>
2010-12-14 15:28:12 -0300

    try using gyroscope -> all worng 	addin 4 some math

--- IMUStabilizedLaserRangeFinder/Main.cs
+++ IMUStabilizedLaserRangeFinder/Main.cs
@@ -6,6 +6,7 @@ namespace IMUStabilizedLaserRangeFinder
 {
 	class MainClass
 	{
+		static ImprovedSpatial spatial = new ImprovedSpatial();
 		public static void Main (string[] args)
 		{
 			Console.WriteLine ("Hello World!");
@@ -16,9 +17,35 @@ namespace IMUStabilizedLaserRangeFinder
 				ServoInitialization();
 				
 				Console.WriteLine("Press any key to end");
-				
-				GoStable();
-				Console.Read();
+				spatial.Initialization();
+				spatial.SpatialAccChanged += new SpatialAccChangeEventHandler(GoStable);
+//				GoStable();	
+//					servo.servos[0].Position = 60;
+//					System.Threading.Thread.Sleep(1000);
+//				
+//				for(double pos =  60 ; pos <130  ;pos++)
+//				{
+//					System.Threading.Thread.Sleep(1000);
+//					servo.servos[0].Position = pos;
+//					Console.WriteLine("{0} {1}", pos, pitch);
+//				}
+				char ch; 
+			
+				do { 
+					ch = (char) System.Console.Read(); 
+					switch(ch){
+					case 'l':
+						//					Console.WriteLine("asdfafsa");
+						break;
+						
+					default:
+						
+						break;
+					}
+				} while(ch != 'q'); 
+				//				Console.Read();
+			
+				spatial.Close();
 				ServoStop();
 				
 				
@@ -30,13 +57,40 @@ namespace IMUStabilizedLaserRangeFinder
 			}
 		}
 		static Servo servo;
-		static double accx,accy,accz;
+//		static double accx,accy,accz;
 		
+		static double pitch =0;
+		static double currentPosition =0;
+		static double th = 1;
 		
-		static void GoStable()
+		static void GoStable(object  sender, SpatialAccEventArgs e)
 		{
-			Console.WriteLine("tring!!!");
+//			Console.WriteLine(e.Pitch);
+			try{
+				pitch = e.Pitch;
 			
+				if(pitch > (-1)*th && pitch < th)
+				{
+					Console.WriteLine("Stable");
+				}
+				else
+				{
+					if( pitch > (-1)*th) // move up
+					{
+						servo.servos[0].Position += Math.Abs(pitch);
+						Console.WriteLine("Off set down {0} ", Math.Abs(pitch));
+						System.Threading.Thread.Sleep(400);
+					}
+					
+					if( pitch < th){
+						Console.WriteLine("Off set up {0} ", Math.Abs(pitch));
+						System.Threading.Thread.Sleep(400);
+					
+						servo.servos[0].Position -= Math.Abs(pitch);		
+					}
+				}
+			}
+			catch(Exception ex) {Console.WriteLine(ex.Message);}
 		}
 		
 		#region Servo
@@ -85,7 +139,8 @@ namespace IMUStabilizedLaserRangeFinder
         //its new position value to the console
         static void servo_PositionChange(object sender, PositionChangeEventArgs e)
         {
-            Console.WriteLine("Servo {0} Position {1}", e.Index, e.Position);
+//            Console.WriteLine("Servo {0} Position {1}", e.Index, e.Position);
+			currentPosition = e.Position;
         }
 		#endregion
 		
